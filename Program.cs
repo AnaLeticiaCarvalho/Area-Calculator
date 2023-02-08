82faf9751294378883c6ace26f0699fc6e774aa2
Ana Leticia Carvalho <analeticiacarvalho01@gmail.com>
2023-02-08 19:49:29 -0300

    area calculator

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿double largura, comprimento, precoMquadrado, area, preco;
+
+Console.WriteLine("Digite a largura do terreno:");
+largura = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o comprimento do terreno:");
+comprimento = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o valor do metro quadrado:");
+precoMquadrado = double.Parse(Console.ReadLine());
+
+area = largura * comprimento;
+preco = area * precoMquadrado;
+
+Console.WriteLine("Área = " + area.ToString("f2"));
+Console.WriteLine("Preço = " + preco.ToString("f2"));
